1f2a81ffb1b0d937d0e6d7c9551df961b707ae36
Ndmjohansen <Ndmjohansen@gmail.com>
2023-10-28 22:36:35 +0200

    Update KlatreGPT.py
    Co-authored-by: FalkenFlyver <FalkenFlyver@users.noreply.github.com>

--- KlatreGPT.py
+++ KlatreGPT.py
@@ -53,7 +53,7 @@ class KlatreGPT:
             inner_message = ''
             for match in re.findall(id_pattern, message.content):
                 # print(f"Match: {match}")
-                message.content = re.sub(match, await self.resolve_user_id(match[2:-1], client), message.content)
+                message.content = re.sub(match, await self.resolve_user_id(match[2:-1], client, message), message.content)
                 print(message.content)
             messages = f"\"{message.author.display_name}: {message.content}\"\n" + messages
         # print('Retrieved history')
