0df690dfaee6d5e891c09641e1e2cb6cf9a86c52
Nicklas Johansen <ndmjohansen@gmail.com>
2023-10-29 12:38:22 +0100

    Fjernet konsulentkode

--- KlatreGPT.py
+++ KlatreGPT.py
@@ -53,28 +53,14 @@ class KlatreGPT:
             inner_message = ''
             for match in re.findall(id_pattern, message.content):
                 # print(f"Match: {match}")
-                message.content = re.sub(match, await self.resolve_user_id(match[2:-1], client, message), message.content)
-                print(message.content)
+                message.content = re.sub(match, await self.resolve_user_id(match[2:-1], client), message.content)
+                # print(message.content)
             messages = f"\"{message.author.display_name}: {message.content}\"\n" + messages
         # print('Retrieved history')
         # print(messages)
         return messages
 
     @staticmethod
-    def get_name(member):
-        if not member.nick is None:
-            return member.nick
-        if not member.global_name is None:
-            return member.global_name
-        return member.name
-
-    async def resolve_user_id(self, user_id, client, message):
-        try:
-            user = await message.guild.get_member(user_id)
-            if user is None:  # This happens if you are talking about a discord user that is not on the current server.
-                user = await client.get_user(user_id)
-                return user.display_name
-        except TypeError:
-            user = await client.fetch_user(user_id)
-            return user.display_name
-            #return self.get_name(user)
+    async def resolve_user_id(user_id, client):
+        user = await client.fetch_user(user_id)
+        return user.display_name
