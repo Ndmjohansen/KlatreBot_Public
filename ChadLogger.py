8f71b3ab0f2d5b072a54e619aa992a80c57aa15a
Magnus <FalkenFlyver@users.noreply.github.com>
2023-11-01 22:37:57 +0100

    added more logging

--- ChadLogger.py
+++ ChadLogger.py
@@ -3,9 +3,13 @@ import traceback
 
 
 class LogEntry:
+    count = -1
+
     def __init__(self, message, details=None):
         self.message = message
         self.details = details
+        LogEntry.count += 1
+        self.index = LogEntry.count
 
 
 class ChadLogger:
@@ -21,8 +25,46 @@ class ChadLogger:
         print(message)
         self.log.append(LogEntry(message, details))
 
-    def query_logs(self):
-        return "\n".join([f"{index}: {x.message}" for index, x in enumerate(self.log[-10:])])
+    def query_logs(self, count=10):
+        return_val = "```"
+        return_val += "\n".join([f"{x.index}: {x.message}" for
+                                x in self.log[-int(count):]])
+        return_val += "```"
+        print(f"message log length: {len(return_val)}")
+        return ChadLogger.trim_too_long_messages(return_val)
+
+    def query_detailed_logs(self, count=10):
+        return_val = "```"
+        return_val += "\n".join([f"{x.index}: {x.details}" for
+                                x in self.log[-int(count):]])
+        return_val += "```"
+        print(f"detail log length: {len(return_val)}")
+        return ChadLogger.trim_too_long_messages(return_val)
+
+    def query_specific_log(self, index):
+        return_val = "```"
+        x = self.log[int(index)]
+        return_val += f"{x.index}: {x.message}"
+        return_val += "```"
+        print(f"message log length: {len(return_val)}")
+        return ChadLogger.trim_too_long_messages(return_val)
+
+    def query_specific_detailed_log(self, index):
+        return_val = "```"
+        x = self.log[int(index)]
+        return_val += f"{x.index}: {x.details}"
+        return_val += "```"
+        print(f"detail log length: {len(return_val)}")
+        return ChadLogger.trim_too_long_messages(return_val)
+
+    @staticmethod
+    def trim_too_long_messages(message):
+        return_val = message
+        original_length = len(return_val)
+        if original_length > 1800:
+            return_val = return_val[:1800]
+            return_val += f"\n... message too long: {original_length}```"
+        return return_val
 
     def clear_logs(self):
         self.log = []
@@ -35,8 +77,16 @@ class ChadLogger:
         # Format the exception as a string
         exception = traceback.format_exception(
             exc_type, exc_value, exc_traceback)
-        formatted_exception = "".join(
-            exception
+
+        only_direct_traceback = []
+        for e in exception:
+            if 'The above exception was the direct cause of the following exception:' in e:
+                break
+            else:
+                only_direct_traceback.append(e)
+
+        formatted_exception = "\n".join(
+            only_direct_traceback
         )
 
         # Log the formatted exception
